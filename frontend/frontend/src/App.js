41b676b5f6675a551d5c8dc066cd2781036f49f3
MohammedEsamaldin <mohammedesam.mem@gmail.com>
2022-10-07 18:31:48 +0200

    frontend

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,54 +1,68 @@
-// import logo from './logo.svg';
-import './App.css';
-import { useReactMediaRecorder } from "react-media-recorder";
+import React from 'react';
+import useMediaRecorder from '@wmik/use-media-recorder';
 
-function App() {
-  const record = document.querySelector(".record");
-  const stop = document.querySelector(".stop");
-  const soundClips = document.querySelector(".sound-clips");
-  const mediaRecorder = new MediaRecorder(stream);
-
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    console.log("getUserMedia supported.");
-    navigator.mediaDevices
-      .getUserMedia(
-        // constraints - only audio needed for this app
-        {
-          audio: true,
-        }
-      )
-  
-      // Success callback
-      .then((stream) => {})
-  
-      // Error callback
-      .catch((err) => {
-        console.error(`The following getUserMedia error occurred: ${err}`);
-      });
-  } else {
-    console.log("getUserMedia not supported on your browser!");
+function Player({ srcBlob, audio }) {
+  if (!srcBlob) {
+    return null;
   }
-  record.onclick = () => {
-    mediaRecorder.start();
-    console.log(mediaRecorder.state);
-    console.log("recorder started");
-    record.style.background = "red";
-    record.style.color = "black";
-  };
-  let chunks = [];
 
-mediaRecorder.ondataavailable = (e) => {
-  chunks.push(e.data);
-};
-
+  if (audio) {
+    return <audio src={URL.createObjectURL(srcBlob)} controls />;
+  }
 
   return (
-    <div className="App">
-      <p>{status}</p>
-      <button onClick={startRecording}>Start Recording</button>
-      <button onClick={stopRecording}>Stop Recording</button>
-      <video src={mediaBlobUrl} controls autoPlay loop />
-          </div>
+    <video
+      src={URL.createObjectURL(srcBlob)}
+      width={520}
+      height={480}
+      controls
+    />
   );
 }
-export default App;
+
+function ScreenRecorderApp() {
+  let {
+    error,
+    status,
+    mediaBlob,
+    stopRecording,
+    getMediaStream,
+    startRecording
+  } = useMediaRecorder({
+    recordScreen: true,
+    blobOptions: { type: 'video/webm' },
+    mediaStreamConstraints: { audio: true, video: true }
+  });
+
+  return (
+    <article>
+      <h1>Screen recorder</h1>
+      {error ? `${status} ${error.message}` : status}
+      <section>
+        <button
+          type="button"
+          onClick={getMediaStream}
+          disabled={status === 'ready'}
+        >
+          Share screen
+        </button>
+        <button
+          type="button"
+          onClick={startRecording}
+          disabled={status === 'recording'}
+        >
+          Start recording
+        </button>
+        <button
+          type="button"
+          onClick={stopRecording}
+          disabled={status !== 'recording'}
+        >
+          Stop recording
+        </button>
+      </section>
+      <Player srcBlob={mediaBlob} />
+    </article>
+  );
+}
+export default ScreenRecorderApp();
