0c2ca8d70e437135c055666adaa8f2b08b0fa9fb
MohammedEsamaldin <mohammedesam.mem@gmail.com>
2022-10-07 16:19:28 +0200

    start recored button

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -3,9 +3,45 @@ import './App.css';
 import { useReactMediaRecorder } from "react-media-recorder";
 
 function App() {
-  const RecordView = () => {
-    const { status, startRecording, stopRecording, mediaBlobUrl } =
-      useReactMediaRecorder({ video: true });
+  const record = document.querySelector(".record");
+  const stop = document.querySelector(".stop");
+  const soundClips = document.querySelector(".sound-clips");
+  const mediaRecorder = new MediaRecorder(stream);
+
+  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    console.log("getUserMedia supported.");
+    navigator.mediaDevices
+      .getUserMedia(
+        // constraints - only audio needed for this app
+        {
+          audio: true,
+        }
+      )
+  
+      // Success callback
+      .then((stream) => {})
+  
+      // Error callback
+      .catch((err) => {
+        console.error(`The following getUserMedia error occurred: ${err}`);
+      });
+  } else {
+    console.log("getUserMedia not supported on your browser!");
+  }
+  record.onclick = () => {
+    mediaRecorder.start();
+    console.log(mediaRecorder.state);
+    console.log("recorder started");
+    record.style.background = "red";
+    record.style.color = "black";
+  };
+  let chunks = [];
+
+mediaRecorder.ondataavailable = (e) => {
+  chunks.push(e.data);
+};
+
+
   return (
     <div className="App">
       <p>{status}</p>
@@ -15,5 +51,4 @@ function App() {
           </div>
   );
 }
-
 export default App;
