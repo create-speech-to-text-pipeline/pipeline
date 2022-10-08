b957cd91798880320a97a9d7adf288298b5db619
nahomHmichael <nahomhabtemichael@gmail.com>
2022-10-08 19:37:17 +0300

    add noise removing func

--- scripts/noise_reduction.py
+++ scripts/noise_reduction.py
@@ -4,3 +4,14 @@ import IPython
 import soundfile as sf
 import matplotlib.pyplot as plt
 import librosa
+from tempfile import SpooledTemporaryFile
+import noisereduce as nr
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
+
+def remove_noise(audio_data):
+    audio, sr = librosa.load(audio_data, sr=8000, mono=True)
+    clean_audio = nr.reduce_noise(audio_data, sr=sr,
+                                  n_std_thresh_stationary=1.5, stationary=True)
+
+    return clean_audio
