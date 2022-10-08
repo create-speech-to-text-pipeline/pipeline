8c4e3009fa520e36a8c01e6246ae0f9e4ef04935
nahomHmichael <nahomhabtemichael@gmail.com>
2022-10-08 20:22:24 +0300

    Update

--- scripts/noise_reduction.py
+++ scripts/noise_reduction.py
@@ -2,7 +2,6 @@ import urllib.request
 import io
 import IPython
 import soundfile as sf
-import matplotlib.pyplot as plt
 import librosa
 from tempfile import SpooledTemporaryFile
 import noisereduce as nr
@@ -15,3 +14,12 @@ def remove_noise(audio_data):
                                   n_std_thresh_stationary=1.5, stationary=True)
 
     return clean_audio
+
+def remove_silence(audio_data):
+    audio, sr = librosa.load(audio_data, sr=8000, mono=True)
+    clips = librosa.effects.split(audio, top_db=10)
+    wav_data = []
+    for c in clips:
+        data = audio[c[0]: c[1]]
+        wav_data.extend(data)
+    return wav_data
